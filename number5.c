be83f85a429b6998b4fcc532e51860fb36fb2a4a
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-01-24 07:15:55 +0530

    c program
    number

---
+++ number5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num,i=1;
+printf("Enter any number:");
+scanf("%d",&num);
+printf("Multiplication table of%d:",num);
+while(i<=10)
+{
+printf("%dx%d=%d",num,i,num*i);
+i++;
+}
+return 0;
+}
