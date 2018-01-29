bda6ecef6837e331f8d77448e0153a16af4deae1
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-01-29 21:35:54 +0530

    c
    hours

---
+++ hours.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int hours,minute,minute 1,total;
+printf("hrs:");
+if(scanf("%d",&hours)==1)
+{
+printf("mins:");
+
+if(scanf("%d",&minutes)==1)
+{
+minute1=hours*60;
+total=minutes+minute1;
+printf("total minutes=>%d",totl);
+}
+else
+{
+printf("error,enter correct values");
+}
+else
+{
+printf("error,enter the correct value");
+}
+}
