b45cd8853af56b3999d4561dda3d79fd7d12ccb5
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-02-05 15:46:14 +0530

    c
    swapping of two numbers using bitwise operator

---
+++ swapping.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2;
+printf("\n enter two numbers:");
+scanf("%d%d",&num1,&num2);
+printf("the number before,num1,num2);
+swap(&num1,&num2);
+return 0;
+}
