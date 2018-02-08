4a77f5e77c225e832f42fc741f6d3bc97dfa4b8d
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-02-08 20:22:24 +0530

    c
    number

---
+++ RANGE.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("\n Enter the number");
+if(n>=1&&n<=10)
+{
+printf("\n it range from[1-10]");
+}
+else
+{
+printf("\n not include");
+}
+getch();
+}
