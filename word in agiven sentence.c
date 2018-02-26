487e2ddc68ba98dc3597bfed12fb5cf00566ed18
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-02-26 02:32:50 -0800

    c program
    number of words in agiven sentence

---
+++ word in agiven sentence.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char str[50];
+int i,count=1;
+printf("Enter the sentence:");
+scanf("%s[^\n]",str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]=='')
+{
+count=count+1;
+}
+}
+printf("\n number of words %d",count);
+return 0;
+}
