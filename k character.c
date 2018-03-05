eadc12e76520cff4595e4bc1660a54ff487b8ae0
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-03-05 02:34:01 -0800

    c program
    a number k print the first k character

---
+++ k character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char str[20];
+int k,i;
+printf("Enter the string and then k value:");
+scanf(%s%d",str,&k);
+for(i=0;i<k;i++)
+{
+printf("\n %c",str[i]);
+}
+return 0;
+}
