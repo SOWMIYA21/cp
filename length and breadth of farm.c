34d73dcf3b8907716dbe5c4fc2a9a82531dfb78e
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-03-05 02:43:14 -0800

    c program
    length and breadth of farm

---
+++ length and breadth of farm.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float length,breadth,area;
+printf("Enter the length and breadth value of farm:");
+scanf("%f%f",&length,&breadth);
+area=length*breadth;
+printf("\n %f 0.5",area);
+return 0;
+}
